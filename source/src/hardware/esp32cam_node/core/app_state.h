a0ef437614d0cb95b348431250acafae6faa7b82
Quang-Luu-2005 <162467787+Quang-Luu-2005@users.noreply.github.com>
2026-07-11 09:17:09 +0700

    fix: update include paths for face recognition and detection models

--- source/src/hardware/esp32cam_node/core/app_state.h
+++ source/src/hardware/esp32cam_node/core/app_state.h
@@ -3,9 +3,9 @@
 #include <Arduino.h>
 #include <WebServer.h>
 
-#include "face_recognition_112_v1_s8.hpp"
-#include "human_face_detect_mnp01.hpp"
-#include "human_face_detect_msr01.hpp"
+#include "model_zoo/face_recognition_112_v1_s8.hpp"
+#include "model_zoo/human_face_detect_mnp01.hpp"
+#include "model_zoo/human_face_detect_msr01.hpp"
 
 extern WebServer webServer;
 extern FaceRecognition112V1S8 recognizer;
